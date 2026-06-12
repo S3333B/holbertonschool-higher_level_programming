c5542189710e2b6599e70896740026c1c7b790f1
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-06-12 11:31:54 +0200

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""List all states starting with N from a MySQL database."""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states "
+        "WHERE name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
