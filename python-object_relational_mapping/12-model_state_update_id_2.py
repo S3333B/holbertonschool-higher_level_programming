49d25f9fde9e04849ad863cc26963f49d32f16cd
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-06-12 11:40:22 +0200

    12-model_state_update_id_2.py

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Update the State object with id 2 using SQLAlchemy."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state is not None:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
